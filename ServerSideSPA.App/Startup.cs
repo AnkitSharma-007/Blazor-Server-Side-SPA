894d3d33340365aa4f9095dcb740ec7ec5715877
Ankit <33789321+AnkitSharma-007@users.noreply.github.com>
2018-08-24 17:34:24 +0530

    Update Startup.cs

--- ServerSideSPA.App/Startup.cs
+++ ServerSideSPA.App/Startup.cs
@@ -10,7 +10,6 @@ namespace ServerSideSPA.App
         {
             // Since Blazor is running on the server, we can use an application service
             // to read the forecast data.
-            services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<EmployeeService>();
         }
 
